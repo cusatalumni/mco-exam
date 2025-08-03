086d76e0d6930ed3d4a9fff7fcc9eb8a24350f93
cusatalumni <cusatalumni@gmail.com>
2025-08-03 15:35:10 +0400

    replace

--- pages/api/submit-test.ts
+++
@@ -1,7 +0,0 @@
-// /pages/api/some-api.ts
-import { redis } from '../../lib/redis';
-
-export default async function handler(req, res) {
-  const value = await redis.get('foo');
-  res.status(200).json({ value });
-}
